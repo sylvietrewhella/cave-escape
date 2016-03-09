d2d53b1810c8cd67693658f53cf2a34a70b971a6
Jake Renzella <j@kerenzella.com>
2016-03-09 13:13:11 +1100

    Add iteration files

---
+++ SwimmingTurtle/src/CaveEscape2.pas
@@ -0,0 +1,43 @@
+program GameMain;
+uses SwinGame, sgTypes, sgTimers, sgSprites, sysUtils;
+
+procedure LoadResources();
+begin
+  LoadResourceBundleNamed('CaveEscape', 'CaveEscape.txt', false);
+  LoadBitmapNamed('background', 'background.png');
+end;
+
+function GetNewPlayer(): Sprite;
+begin
+	result := CreateSprite(BitmapNamed('Player'), AnimationScriptNamed('PlayerAnimations'));
+	SpriteSetX(result, ScreenWidth() / 2 - SpriteWidth(result));
+	SpriteSetY(result, ScreenHeight() / 2);
+	SpriteStartAnimation(result, 'fly');
+end;
+
+procedure Main();
+var
+  player: Sprite;
+begin
+  OpenGraphicsWindow('Cave Escape', 432, 768);
+	OpenAudio();
+  LoadResources();
+
+  player := GetNewPlayer();
+
+  repeat // The game loop...
+    ProcessEvents();
+		ClearScreen(ColorWhite);
+
+		UpdateSprite(player);
+    DrawSprite(player);
+
+
+    RefreshScreen();
+
+  until WindowCloseRequested();
+end;
+
+begin
+  Main();
+end.
---
+++ SwimmingTurtle/src/CaveEscape3.pas
@@ -0,0 +1,68 @@
+program GameMain;
+uses SwinGame, sgTypes, sgTimers, sgSprites, sysUtils;
+
+const
+  GRAVITY = 0.08;
+  JUMP_RECOVERY_BOOST = 2;
+  MAX_SPEED = 5;
+  MAX_ROTATION_ANGLE = 90;
+  FOREGROUND_SCROLL_SPEED = 2;
+  BACKGROUND_SCROLL_SPEED = 1;
+  SPRITE_FRAME_DURATION = 150;
+
+procedure LoadResources();
+begin
+  LoadResourceBundleNamed('CaveEscape', 'CaveEscape.txt', false);
+  LoadBitmapNamed('background', 'background.png');
+end;
+
+function GetNewPlayer(): Sprite;
+begin
+	result := CreateSprite(BitmapNamed('Player'), AnimationScriptNamed('PlayerAnimations'));
+	SpriteSetX(result, ScreenWidth() / 2 - SpriteWidth(result));
+	SpriteSetY(result, ScreenHeight() / 2);
+	SpriteStartAnimation(result, 'fly');
+end;
+
+procedure UpdateVelocity(var toUpdate: Sprite);
+begin
+	SpriteSetDy(toUpdate, SpriteDy(toUpdate) + GRAVITY);
+
+	if SpriteDy(toUpdate) > MAX_SPEED then
+	begin
+		SpriteSetDy(toUpdate, MAX_SPEED);
+	end
+	else if (SpriteDy(toUpdate) < MAX_SPEED * -1) then
+	begin
+	SpriteSetDy(toUpdate,  MAX_SPEED * -1);
+	end;
+end;
+
+procedure Main();
+var
+  player: Sprite;
+begin
+  OpenGraphicsWindow('Cave Escape', 432, 768);
+	OpenAudio();
+  LoadResources();
+
+  player := GetNewPlayer();
+
+  repeat // The game loop...
+    ProcessEvents();
+		ClearScreen(ColorWhite);
+
+    UpdateVelocity(player);
+		UpdateSprite(player);
+
+    DrawSprite(player);
+
+
+    RefreshScreen();
+
+  until WindowCloseRequested();
+end;
+
+begin
+  Main();
+end.
