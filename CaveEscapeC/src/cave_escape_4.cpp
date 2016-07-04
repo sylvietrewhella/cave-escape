82f81660f98144ff4d14c395ae38dd0677d40c56
Reuben Wilson <reuben@bitnak.com>
2016-07-05 09:19:57 +1000

    ADD: Add fourth iteration in C

---
+++ CaveEscapeC/src/cave_escape_4.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "SwinGame.h"
+
+#define GRAVITY 0.08
+#define MAX_SPEED 5
+#define JUMP_RECOVERY_BOOST 2
+
+sprite get_new_player()
+{
+  sprite result;
+  result = create_sprite(bitmap_named("Player"), animation_script_named("PlayerAnimations"));
+  sprite_set_x(result, screen_width() / 2 - sprite_width(result));
+  sprite_set_y(result, screen_height() / 2);
+  sprite_start_animation(result, "Fly");
+
+  return result;
+}
+
+void handle_input(sprite player)
+{
+  if (key_typed(SPACE_KEY))
+  {
+    sprite_set_dy(player, sprite_dy(player) - JUMP_RECOVERY_BOOST);
+  }
+}
+
+void update_velocity(sprite player)
+{
+  sprite_set_dy(player, sprite_dy(player) + GRAVITY);
+
+  if (sprite_dy(player) > MAX_SPEED)
+  {
+    sprite_set_dy(player, MAX_SPEED);
+  }
+  else if (sprite_dy(player) < -(MAX_SPEED))
+  {
+    sprite_set_dy(player, -(MAX_SPEED));
+  }
+}
+
+int main()
+{
+    sprite player;
+
+    open_graphics_window("Cave Escape", 432, 768);
+    load_resource_bundle_named("CaveEscape", "CaveEscape.txt", false);
+
+    player = get_new_player();
+
+    do
+    {
+      process_events();
+      clear_screen(ColorWhite);
+
+      update_velocity(player);
+      handle_input(player);
+      update_sprite(player);
+
+      draw_sprite(player);
+
+      refresh_screen();
+
+    } while(!window_close_requested());
+
+    return 0;
+}
