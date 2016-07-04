a55f0bf5029a7e0d2945798bc9c7b90758d7749d
Reuben Wilson <reuben@bitnak.com>
2016-07-05 09:06:54 +1000

    ADD: Add second iteration in C

---
+++ CaveEscapeC/src/cave_escape_2.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "SwinGame.h"
+
+void load_resources()
+{
+  load_resource_bundle_named("CaveEscape", "CaveEscape.txt", false);
+}
+
+sprite get_new_player()
+{
+  sprite result;
+  result = create_sprite(bitmap_named("Player"), animation_script_named("PlayerAnimations"));
+  sprite_set_x(result, screen_width() / 2 - sprite_width(result));
+  sprite_set_y(result, screen_height() / 2);
+  sprite_start_animation(result, "Fly");
+
+  return result;
+}
+
+int main()
+{
+    sprite player;
+
+    open_graphics_window("Cave Escape", 432, 768);
+    load_resources();
+
+    player = get_new_player();
+
+    do
+    {
+      process_events();
+      clear_screen(ColorWhite);
+
+      update_sprite(player);
+      draw_sprite(player);
+
+      refresh_screen();
+
+    } while(!window_close_requested());
+
+    return 0;
+}
